469cb44a44f6427a9c99769970d95ff264bda287
chenyongliang@foresee.com.cn <chenyongliang@foresee.com.cn>
2022-04-06 23:21:04 +0800

    Flowable与Spring Boot集成

---
+++ src/main/java/cyl/learn/flowable/FlowableSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package cyl.learn.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableSpringbootDemoApplication.class, args);
+    }
+
+}
