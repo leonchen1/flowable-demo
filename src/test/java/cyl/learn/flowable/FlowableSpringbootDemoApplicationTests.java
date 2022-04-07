818b80938ca198c5bfbfc61156701d2d8611a9ed
leonchen1 <728974047@qq.com>
2022-04-08 00:15:24 +0800

    删除 .properties 文件，使用 .yaml 文件

--- src/test/java/cyl/learn/flowable/FlowableSpringbootDemoApplicationTests.java
+++ src/test/java/cyl/learn/flowable/FlowableSpringbootDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package cyl.learn.flowable;
 
+import org.flowable.engine.ProcessEngine;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlowableSpringbootDemoApplicationTests {
 
+    @Qualifier("processEngine")
+    @Autowired
+    public ProcessEngine processEngine;
+
     @Test
     void contextLoads() {
+        System.out.println(processEngine);
     }
 
 }
