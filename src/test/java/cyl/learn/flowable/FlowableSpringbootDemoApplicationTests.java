ffe1a6130130fbf996fabd73d13c36eb140dc50d
leonchen1 <728974047@qq.com>
2022-04-08 22:07:22 +0800

    添加手动部署 BPMN 2.0 流程定义文件测试样例

--- src/test/java/cyl/learn/flowable/FlowableSpringbootDemoApplicationTests.java
+++ src/test/java/cyl/learn/flowable/FlowableSpringbootDemoApplicationTests.java
@@ -1,21 +1,31 @@
 package cyl.learn.flowable;
 
-import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.repository.Deployment;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlowableSpringbootDemoApplicationTests {
 
-    @Qualifier("processEngine")
     @Autowired
-    public ProcessEngine processEngine;
+    public RepositoryService repositoryService;
 
+    /**
+     * Springboot 集成 flowable 后：
+     * processes目录下的任何BPMN 2.0流程定义都会被自动部署，
+     * cases目录下的任何CMMN 1.1事例都会被自动部署，
+     * forms目录下的任何Form定义都会被自动部署。
+     * 当前方法演示手动部署 BPMN 2.0 流程定义
+     */
     @Test
-    void contextLoads() {
-        System.out.println(processEngine);
+    void testManualDeploy() {
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .name("holiday-request")
+                .deploy();
+        System.out.println("deployment.getId() = " + deployment.getId());
+        System.out.println("deployment.getName() = " + deployment.getName());
     }
-
 }
